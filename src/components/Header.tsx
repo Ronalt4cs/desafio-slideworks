a753fb28bc5edd65f50c98b63ffb8d66e1bc5f39
ronalt augusto <ronaltaugustocs@gmmail.com>
2025-04-15 17:51:33 -0300

    chore: remove unused function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,6 @@
-import { MoviesApiResponse } from "@/types/movie";
 import Image from "next/image";
 
 export async function Header() {
-
-  const res = await fetch("https://movies.slideworks.cc/movies/?page&limit=3");
-  const movies: MoviesApiResponse = await res.json();
-
   return (
     <header className="w-full h-28 pl-32 flex items-center max-lg:justify-center max-lg:pl-0">
       <Image
